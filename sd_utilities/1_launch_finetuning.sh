7e2505119fd1a7db81f6101c10ebaf1b6dcdec77
Luigi Capogrosso <luigi@capogrosso.eu>
2024-06-01 17:07:38 +0100

    Add the website

--- sd_utilities/1_launch_finetuning.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-accelerate launch \
---config_file=configs/accelerate_config.yaml \
-train_network.py \
---dataset_config=configs/dataset_config.toml \
---config_file=configs/training_config.toml
--- sd_utilities/2_launch_merge_lora.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python networks/merge_lora.py \
---sd_model models/sd-v1-5-pruned-noema-fp16.safetensors \
---models output/SD_MODEL_PATH.safetensors \
---save_to output/lora/SD_MODEL_W_LORA_PATH.safetensors \
---ratios 0.65 # How the weights are distributed between the two models.
